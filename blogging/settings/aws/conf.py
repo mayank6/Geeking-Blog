b70ddcfab3affaade92eb63eebc8ac31c3cee915
Mayank Chauhan <chauhanmayank.621@gmail.com>
2018-09-04 21:09:30 +0530

    Initial commit

--- blogging/settings/aws/conf.py
+++ blogging/settings/aws/conf.py
@@ -1,4 +1,5 @@
-import datetime
+import datetime,os
+
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_FILE_EXPIRE = 200
