2402a37a9fbf8b070e0f4302ea7f35677ee0c962
Mayank Chauhan <chauhanmayank.621@gmail.com>
2018-09-04 21:33:23 +0530

    Initial commit

--- blogging/settings/production.py
+++ blogging/settings/production.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'blogging.wsgi.application'
 
 
 DATABASES = {
-    # 'default': {
+     'default': {}
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
